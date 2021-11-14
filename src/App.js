744ee886c0674cbd464c409b1d5ec8c71ab93bf6
nouria chaouy <nouria.chaouy@gmail.com>
2021-11-14 07:59:44 +0000

    called API and added height functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FlashcardList from './FlashcardList';
 import './App.css';
+import axios from 'axios';
 
 
 function App() {
   const [flashcards, setFlashcards ] = useState(SAMPLE_FLASHCARDS);
-  return (
-   <FlashcardList flashcards={flashcards} />
 
+  useEffect(() => {
+    axios
+    .get('https://opentdb.com/api.php?amount=10')
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer);
+        const options = [...questionItem.incorrect_answers.map(a => decodeString(a)), answer];
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: answer,
+          options: options.sort(() => Math.random() * .5)
+        }
+      }))
+    })
+  }, [])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+    <form className="header" onSubmit={handleSubmit}>
+
+    </form>
+    <div className="container">
+      <FlashcardList flashcards={flashcards} />
+    </div>
+    </>
   );
 }
 
